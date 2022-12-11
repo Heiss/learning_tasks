f21de058e98cd3d8f01563ad52a90863853a31d5
Heiss <physicx@netzmuffel.de>
2022-12-11 13:19:42 +0100

    add day11 aoc22

--- Rust/adventofcode22/src/main.rs
+++ Rust/adventofcode22/src/main.rs
@@ -1,22 +1,5 @@
-use adventofcode::{day1, day10, day2, day3, day4, day5, day6, day7, day8};
+use adventofcode::run;
 
 fn main() {
-    let input = include_str!("./day1.txt");
-    day1(input);
-    let input = include_str!("./day2.txt");
-    day2(input);
-    let input = include_str!("./day3.txt");
-    day3(input);
-    let input = include_str!("./day4.txt");
-    day4(input);
-    let input = include_str!("./day5.txt");
-    day5(input);
-    let input = include_str!("./day6.txt");
-    day6(input);
-    let input = include_str!("./day7.txt");
-    day7(input);
-    let input = include_str!("./day8.txt");
-    day8(input);
-    let input = include_str!("./day10.txt");
-    day10(input);
+run();
 }
