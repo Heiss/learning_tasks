7dd14844c7ae247c5e1c9a1b7bcaab0ac37e2c86
Peter Heiss <peter.heiss@uni-muenster.de>
2023-12-02 20:17:54 +0100

    add day2 for aoc23

---
+++ Rust/adventofcode23/src/day3.rs
@@ -0,0 +1,29 @@
+fn part1(input: &str) -> usize {
+    todo!("Implement part 1");
+}
+
+fn part2(input: &str) -> usize {
+    todo!("Implement part 2");
+}
+
+pub fn day() {
+    let input = include_str!("../input/day3.txt");
+    print!("Day 3\t");
+    print!("Part 1: {}\t", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = r#""#;
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 8);
+    }
+
+    #[test]
+    fn test_test2() {
+        assert_eq!(part2(INPUT), 2286);
+    }
+}
