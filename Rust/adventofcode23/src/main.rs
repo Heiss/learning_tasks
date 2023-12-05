5d788bab0f1e2e33a5529a66258c079061038a15
Peter Heiss <peter.heiss@uni-muenster.de>
2023-12-05 21:54:06 +0100

    add parallel exec for day5 and time measurement

--- Rust/adventofcode23/src/main.rs
+++ Rust/adventofcode23/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 mod day1;
 mod day2;
 mod day3;
@@ -5,9 +7,13 @@ mod day4;
 mod day5;
 
 fn main() {
-    day1::day();
-    day2::day();
-    day3::day();
-    day4::day();
-    day5::day();
+    let vec = vec![day1::day, day2::day, day3::day, day4::day, day5::day];
+    std::thread::scope(|s| {
+        vec.iter()
+            .map(|f| (Instant::now(), s.spawn(|| f())))
+            .for_each(|(t, h)| {
+                h.join().unwrap();
+                println!("Time: {:?}", t.elapsed());
+            });
+    });
 }
