8d6ee95a1f4c796a0fe09405a4a4c14cb96616c7
Peter Heiss <peter.heiss@uni-muenster.de>
2023-12-07 10:56:52 +0100

    add day7 of aoc23

--- Rust/adventofcode23/src/main.rs
+++ Rust/adventofcode23/src/main.rs
@@ -4,12 +4,14 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
     day1::day();
     day2::day();
     day3::day();
     day4::day();
-    day5::day();
+//    day5::day();
     day6::day();
+    day7::day();
 }
