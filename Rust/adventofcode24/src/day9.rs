a78d07d9647cebbbe7d2c04a76e6b1f40dd2c029
Peter Heiss <peter.heiss@uni-muenster.de>
2024-12-09 22:46:03 +0100

    add day9 part 1

--- Rust/adventofcode24/src/day9.rs
+++ Rust/adventofcode24/src/day9.rs
@@ -1,5 +1,84 @@
+use std::str::FromStr;
+
+type Id = usize;
+
+#[derive(PartialEq, Debug)]
+enum Block {
+    Free,
+    File(Id),
+}
+
+struct Disk {
+    space: Vec<Block>,
+}
+
+impl Disk {
+    fn reduction(mut self) -> ReducedDisk {
+        let mut current_right_ptr = self.space.len() - 1;
+        for current_left_ptr in 0..self.space.len() {
+            if current_left_ptr >= current_right_ptr {
+                break;
+            }
+
+            if self.space[current_left_ptr] != Block::Free {
+                continue;
+            }
+
+            self.space.swap(current_left_ptr, current_right_ptr);
+            while self.space[current_right_ptr] == Block::Free {
+                current_right_ptr -= 1;
+            }
+        }
+        ReducedDisk { disk: self }
+    }
+}
+
+impl FromStr for Disk {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let res = s
+            .char_indices()
+            .flat_map(|(i, c)| {
+                let n: usize = c.to_digit(10).unwrap() as usize;
+                (0..n)
+                    .map(|_| {
+                        if i % 2 == 0 {
+                            Block::File(i / 2)
+                        } else {
+                            Block::Free
+                        }
+                    })
+                    .collect::<Vec<Block>>()
+            })
+            .collect();
+        Ok(Self { space: res })
+    }
+}
+
+struct ReducedDisk {
+    disk: Disk,
+}
+
+impl ReducedDisk {
+    fn checksum(&self) -> usize {
+        self.disk
+            .space
+            .iter()
+            .enumerate()
+            .map(|(i, n)| {
+                if let Block::File(num) = *n {
+                    i * num
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+}
+
 fn part1(input: &str) -> usize {
-    0
+    Disk::from_str(input).unwrap().reduction().checksum()
 }
 
 fn part2(_input: &str) -> usize {
@@ -22,6 +101,6 @@ mod tests {
     }
     #[test]
     fn day2() {
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), 258);
     }
 }
