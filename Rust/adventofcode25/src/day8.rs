cad7ae24fa05832cce4622ebaa672f6be9424fe0
Peter Heiss <peter.heiss@uni-muenster.de>
2025-12-18 11:46:17 +0100

    performance increase with ai

--- Rust/adventofcode25/src/day8.rs
+++ Rust/adventofcode25/src/day8.rs
@@ -1,28 +1,29 @@
-use std::fmt::Display;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::mem::swap;
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
+
 use itertools::Itertools;
 
 struct DSU<T> {
-    nodes: Vec<Arc<Mutex<DSUNode<T>>>>,
-    sets: Vec<Arc<Mutex<DSUNode<T>>>>,
-    distances: Vec<(usize, usize, usize)>,
+    nodes: Vec<DSUNode<T>>,
+    num_sets: usize,
+    distances: BinaryHeap<Reverse<(usize, usize, usize)>>,
 }
 
 #[derive(Debug)]
 struct DSUNode<T> {
     rank: usize,
-    parent: Option<Arc<Mutex<DSUNode<T>>>>,
+    parent: usize,
     value: T,
     count_children: usize,
 }
 
 impl<T> DSUNode<T> {
-    fn create(value: T) -> DSUNode<T> {
+    fn create(value: T, self_index: usize) -> DSUNode<T> {
         Self {
             rank: 0,
-            parent: None,
+            parent: self_index,
             count_children: 1,
             value,
         }
@@ -38,10 +39,9 @@ type Point3 = (usize, usize, usize);
 
 impl Distance<Point3> for Point3 {
     fn distance(&self, o: &Point3) -> usize {
-        ((self.0.abs_diff(o.0)).pow(2)
+        (self.0.abs_diff(o.0)).pow(2)
             + (self.1.abs_diff(o.1)).pow(2)
-            + (self.2.abs_diff(o.2)).pow(2))
-        .isqrt()
+            + (self.2.abs_diff(o.2)).pow(2)
     }
 
     fn get_x(&self) -> usize {
@@ -49,12 +49,12 @@ impl Distance<Point3> for Point3 {
     }
 }
 
-impl<T: Distance<T> + std::fmt::Debug> DSU<T> {
+impl<T: Distance<T> + std::fmt::Debug + Copy> DSU<T> {
     fn new() -> DSU<T> {
         DSU {
             nodes: Vec::new(),
-            sets: Vec::new(),
-            distances: Vec::new(),
+            num_sets: 0,
+            distances: BinaryHeap::new(),
         }
     }
 
@@ -63,76 +63,66 @@ impl<T: Distance<T> + std::fmt::Debug> DSU<T> {
     }
 
     fn calculate_distances(&mut self) {
-        let mut distances: Vec<(usize, usize, usize)> = (0..self.nodes.len())
+        self.distances = (0..self.nodes.len())
             .combinations(2)
             .map(|pair| {
                 let i = pair[0];
                 let j = pair[1];
-                let node_i_value = &self.nodes[i].lock().unwrap().value;
-                let node_j_value = &self.nodes[j].lock().unwrap().value;
+                let node_i_value = &self.nodes[i].value;
+                let node_j_value = &self.nodes[j].value;
                 let distance = node_i_value.distance(node_j_value);
-                (i, j, distance)
+                Reverse((distance, i, j))
             })
             .collect();
-
-        // Sort by distance in descending order.
-        distances.sort_by(|a, b| b.2.cmp(&a.2));
-
-        self.distances = distances;
     }
 
     fn pop_smallest_distances(&mut self) -> Option<(usize, usize, usize)> {
-        self.distances.pop()
+        self.distances.pop().map(|r| (r.0.1, r.0.2, r.0.0))
     }
 
-    fn make_set(&mut self, value: T) -> Arc<Mutex<DSUNode<T>>> {
-        let node = Arc::new(Mutex::new(DSUNode::create(value)));
-        node.lock().unwrap().parent = Some(node.clone());
-        self.nodes.push(node.clone());
-        self.sets.push(node.clone());
-        node
+    fn make_set(&mut self, value: T) {
+        let index = self.nodes.len();
+        let node = DSUNode::create(value, index);
+        self.nodes.push(node);
+        self.num_sets += 1;
     }
 
-    fn union(&mut self, a: Arc<Mutex<DSUNode<T>>>, b: Arc<Mutex<DSUNode<T>>>) {
-        self.link(self.find_set(a), self.find_set(b));
-    }
+    fn union(&mut self, i: usize, j: usize) {
+        let mut root_i = self.find_set(i);
+        let mut root_j = self.find_set(j);
 
-    fn remove(&mut self, node: Arc<Mutex<DSUNode<T>>>) {
-        for i in 0..self.sets.len() {
-            if Arc::ptr_eq(&node, &self.sets[i]) {
-                self.sets.remove(i);
-                break;
+        if root_i != root_j {
+            if self.nodes[root_i].rank < self.nodes[root_j].rank {
+                swap(&mut root_i, &mut root_j);
             }
+            self.nodes[root_j].parent = root_i;
+            self.nodes[root_i].count_children += self.nodes[root_j].count_children;
+
+            if self.nodes[root_i].rank == self.nodes[root_j].rank {
+                self.nodes[root_i].rank += 1;
+            }
+            self.num_sets -= 1;
         }
     }
 
-    fn link(&mut self, a: Arc<Mutex<DSUNode<T>>>, b: Arc<Mutex<DSUNode<T>>>) {
-        let mut a = self.find_set(a);
-        let mut b = self.find_set(b);
-
-        if !Arc::ptr_eq(&a, &b) {
-            let (a_rank, b_rank) = (a.lock().unwrap().rank, b.lock().unwrap().rank);
-            if a_rank < b_rank {
-                swap(&mut a, &mut b);
-            }
-            b.lock().unwrap().parent = Some(a.clone());
-            a.lock().unwrap().count_children += b.lock().unwrap().count_children;
-            self.remove(b.clone());
-
-            if a_rank == b_rank {
-                a.lock().unwrap().rank += 1;
-            }
+    fn find_set(&mut self, i: usize) -> usize {
+        if self.nodes[i].parent == i {
+            return i;
         }
+        self.nodes[i].parent = self.find_set(self.nodes[i].parent);
+        self.nodes[i].parent
     }
 
-    fn find_set(&self, p0: Arc<Mutex<DSUNode<T>>>) -> Arc<Mutex<DSUNode<T>>> {
-        let mut n = p0.lock().unwrap();
-        let parent = n.parent.as_ref().unwrap().clone();
-        if Arc::ptr_eq(&parent, &p0) {
-            return p0.clone();
-        }
-        n.parent = Some(self.find_set(parent));
-        n.parent.as_ref().unwrap().clone()
+    fn get_set_sizes(&mut self) -> Vec<usize> {
+        (0..self.nodes.len()).for_each(|i| {
+            self.find_set(i);
+        });
+        self.nodes
+            .iter()
+            .enumerate()
+            .filter(|(i, node)| node.parent == *i)
+            .map(|(_, node)| node.count_children)
+            .collect()
     }
 }
 
@@ -158,40 +148,26 @@ fn part1(input: &str, num: usize) -> usize {
     let mut nodes = DSU::from_str(input).unwrap();
     nodes.calculate_distances();
     for _i in 0..num {
-        let (i, j, _distance) = &nodes.pop_smallest_distances().unwrap();
-        let nearest = nodes.nodes[*i].clone();
-        let current = nodes.nodes[*j].clone();
-        nodes.union(nearest, current);
+        let (i, j, _distance) = nodes.pop_smallest_distances().unwrap();
+        nodes.union(i, j);
     }
-    nodes.sets.sort_by(|a, b| {
-        a.lock()
-            .unwrap()
-            .count_children
-            .cmp(&b.lock().unwrap().count_children)
-    });
-    nodes
-        .sets
-        .iter()
-        .rev()
-        .take(3)
-        .fold(1, |a, b| a * b.lock().unwrap().count_children)
+    let mut set_sizes = nodes.get_set_sizes();
+    set_sizes.sort_unstable_by(|a, b| b.cmp(a));
+    set_sizes.iter().take(3).product()
 }
 
 fn part2(input: &str) -> usize {
     let mut nodes = DSU::from_str(input).unwrap();
     nodes.calculate_distances();
     let mut last_pop = None;
-    while nodes.sets.len() > 1 {
-        last_pop = nodes.pop_smallest_distances();
-        let (i, j, _distance) = last_pop.unwrap();
-        let nearest = nodes.nodes[i].clone();
-        let current = nodes.nodes[j].clone();
-        nodes.union(nearest, current);
+    while nodes.num_sets > 1 {
+        let (i, j, distance) = nodes.pop_smallest_distances().unwrap();
+        last_pop = Some((i, j, distance));
+        nodes.union(i, j);
     }
-    let x = last_pop.unwrap().0;
-    let y = last_pop.unwrap().1;
-    let x = nodes.nodes[x].clone().lock().unwrap().value;
-    let y = nodes.nodes[y].clone().lock().unwrap().value;
+    let (x_idx, y_idx, _) = last_pop.unwrap();
+    let x = nodes.nodes[x_idx].value;
+    let y = nodes.nodes[y_idx].value;
 
     x.get_x() * y.get_x()
 }
